f0daa4839bcbbbfaf7493e419e9290ad3b6b4712
Oliver Keyes <ironholds@gmail.com>
2014-06-03 16:09:09 -0700

    rpy tweaks and improvements, along with documentation for the function

--- R/rpy.R
+++ R/rpy.R
@@ -1,16 +1,16 @@
-rpy <- function(x, input_file, output_file, script, ...){
+rpy <- function(x, script, ...){
   
   #Create input/output files
   dir_path <- paste("/tmp/ua_", make.names(Sys.time()), sep = "")
   dir.create(dir_path)
-  input_path <- file.path(dir_path, input_file)
-  output_path <- file.path(dir_path, output_file)
+  input_path <- file.path(dir_path, "input.json")
+  output_path <- file.path(dir_path, "output.json")
   
   #Write to input
   cat(toJSON(x = x), file = input_path)
   
   #Run script
-  system(paste("python", script, input_path, output_path))
+  system(paste("python", script, input_path, output_path, ...))
   
   #Return results
   results <- fromJSON(output_path)
