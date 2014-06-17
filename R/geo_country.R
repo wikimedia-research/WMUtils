2979ca5040c8d662a38e40dc797578a2592c1fd3
Oliver Keyes <ironholds@gmail.com>
2014-06-17 10:46:27 -0700

    bugfix

--- R/geo_country.R
+++ R/geo_country.R
@@ -1,7 +1,7 @@
 geo_country <- function(ips){
   
   #If it's bigger than 2m, split and recursively call
-  if(length(user_agents) > 2000000){
+  if(length(ips) > 2000000){
     
     ips <- split(ips, ceiling(seq_along(ips)/2000000))
     results <- unlist(lapply(ips, geo_country))
