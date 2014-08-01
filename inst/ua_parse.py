5077e7246cdf7b6f9a268a142044964d9026234a
Oliver Keyes <ironholds@gmail.com>
2014-08-01 16:25:33 -0700

    pythonification of previously Rified python

--- inst/ua_parse.py
+++ inst/ua_parse.py
@@ -1,6 +1,12 @@
 from ua_parser import user_agent_parser as uap
 import json
-import sys
+import argparse
+
+#Set up argparse
+parser = argparse.ArgumentParser(description="user agent geolocation")
+parser.add_argument("-i", type = str, help = "The input JSON file", required = True, dest = "input")
+parser.add_argument("-o", type = str, help = "The output JSON file", required = True, dest = "output")
+args = parser.parse_args()
 
 #Function for parsing incoming lists of UAs and throwing them into another list
 def parse_ua(x):
@@ -9,23 +15,23 @@ def parse_ua(x):
     if not(isinstance(x,list)):
         x = [x]
         
-    #Construct output list, same length as the input list
-    output_list = range(len(x))
+    #Construct output list
+    output_list = []
     
     #For each entry in the input list, 
-    for i in range(len(x)):
+    for entry in x:
         
         #Retrieve the UA results
-        UA_results = uap.Parse(x[i])
+        UA_results = uap.Parse(entry)
         
         #Limit to things we care about
-        output_list[i] = {'device': UA_results['device']['family'],'os': UA_results['os']['family'], 'browser': UA_results['user_agent']['family'], 'browser_major': UA_results['user_agent']['major'], 'browser_minor': UA_results['user_agent']['minor']}
+        output_list.append({'device': UA_results['device']['family'],'os': UA_results['os']['family'], 'browser': UA_results['user_agent']['family'], 'browser_major': UA_results['user_agent']['major'], 'browser_minor': UA_results['user_agent']['minor']})
     
     #And return
     return output_list
 
 #Import object
-json_data = open(sys.argv[1])
+json_data = open(args.input)
 data = json.load(json_data, strict = False)
 json_data.close()
 
@@ -33,6 +39,6 @@ json_data.close()
 parsed_data = json.dumps(parse_ua(data))
 
 #Write it
-file = open(sys.argv[2], 'w')
+file = open(args.output, 'w')
 file.write(parsed_data)
 file.close()
