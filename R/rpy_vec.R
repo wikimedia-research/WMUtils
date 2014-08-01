2a805a8da1b85f3339d16135db1e5726fb4596a9
Oliver Keyes <ironholds@gmail.com>
2014-08-01 12:53:37 -0700

    moroxygen

--- R/rpy_vec.R
+++ R/rpy_vec.R
@@ -1,3 +1,16 @@
+#'@title
+#'rpy-to-vector
+#'
+#'@description
+#'\code{rpy_vec} converts the results of rpy calls into a vector after sanitising them for missing values
+#'
+#'@param list the rpy-produced list you want converted into a vector
+#'#'
+#'@author Oliver Keyes <okeyes@@wikimedia.org>
+#'
+#'@seealso \code{\link{rpy_df}} for conversion of rpy results into vectors, and \code{\link{rpy}} itself.
+#'@export
+
 rpy_vec <- function(list){
   
   #Handle NULLs
