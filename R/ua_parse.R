4bae6b1d050979197ada3db69af925ebf05fe3ca
ironholds <ironholds@gmail.com>
2014-09-14 09:27:12 -0400

    ua_parse documentation improvements

--- R/ua_parse.R
+++ R/ua_parse.R
@@ -7,14 +7,14 @@
 #' 
 #' @param user_agents A vector of unparsed user agents
 #' @param fields The elements you'd like to return. Options are "device" (the device code, when known),
-#' "os" (the operating system, when known), "browser" (the browser), "browser_major" (the major version of the browser)
-#' and browser_minor (the minor version of the browser)
+#' "os" (the operating system, when known), "browser" (the browser), "major_version" (the major version of the browser)
+#' and minor_version (the minor version of the browser)
 #' 
 #' @author Oliver Keyes <okeyes@@wikimedia.org>
 #' 
 #' @export
 
-ua_parse <- function(user_agents, fields = c("device","os","browser","minor_version","major_version")){
+ua_parse <- function(user_agents, fields = c("device","os","browser","major_version","minor_version")){
   
   #Sanitise
   user_agents <- gsub(x = user_agents, pattern = "\"", replacement = "")
