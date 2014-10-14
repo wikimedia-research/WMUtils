b427438417234b1f99db15ae23d709f2242e90a8
ironholds <ironholds@gmail.com>
2014-10-14 17:32:00 -0400

    format

--- R/log_strptime.R
+++ R/log_strptime.R
@@ -13,6 +13,7 @@
 #'
 #'@seealso \code{\link{hive_query}} for querying the unsampled logs, \code{\link{sampled_logs}}
 #'for querying the sampled logs, and \code{\link{mw_strptime}} for parsing MediaWiki timestamps.
+#'
 #'@export
 log_strptime <- function(x){
   
