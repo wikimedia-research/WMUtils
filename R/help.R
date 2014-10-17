41f640e4f4f5b6dd7fa985d6ff60643e6ff15410
ironholds <ironholds@gmail.com>
2014-10-17 16:37:19 -0400

    add parlapply

--- R/help.R
+++ R/help.R
@@ -53,7 +53,7 @@
 #'@author Oliver Keyes <okeyes@@wikimedia.org>
 #'@docType package
 #'@name WMUtils
-#'@import RMySQL data.table
+#'@import RMySQL data.table parallel
 #'@importFrom Rcpp evalCpp
 #'@useDynLib WMUtils
 #'@aliases WMUtils WMUtils-package
---
+++ R/parlapply.R
@@ -0,0 +1,30 @@
+#'@title parlapply
+#'@description parallelised lapply wrapper
+#'
+#'@details parlapply is a wrapper around mclapply, a parallelised version of lapply. It exists because it's
+#'often convenient to not have to remember whether you should preschedule or set a seed, or how many
+#'cores are okay, or what the damn function for detecting core numbers is, or, or, or.
+#'
+#'Default settings are to not preschedule, set a seed or allow for recursive calls (you cannot
+#'call parlapply inside a parlapply call. That makes everyone sad.) with [total physical cores]/4 cores
+#'used by the process.
+#'
+#'@param X the list to iterate over \code{\link{keysplit}} can be used to conveniently convert a
+#'data.frame or data.table into something parallelisable.
+#'
+#'@param FUN the function you want to run over each element of X.
+#'
+#'@param ... further arguments to pass to FUN
+#'
+#'@return a list containing the results of FUN for each element of X.
+#'
+#'@export
+parlapply <- function(X, FUN, ...){
+  
+  mclapply(X = X, FUN = FUN,
+           mc.preschedule = FALSE,
+           mc.cores = round(detectCores()/4),
+           mc.set.seed = FALSE,
+           mc.allow.recursive = FALSE,
+           ...)
+}
