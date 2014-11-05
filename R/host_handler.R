0eea0890760f2dad64266b44b2fbb716f55a90bf
ironholds <ironholds@gmail.com>
2014-11-05 11:42:02 -0500

    add host_handler

---
+++ R/host_handler.R
@@ -0,0 +1,70 @@
+#'@title host_handler
+#'@description extract the host from a URL
+#'@details \code{host_handler} extracts the host from a URL and, if asked, also strictly filters the resulting hosts,
+#'extracting general organisational handles (for example "Google", or "Reddit").
+#'
+#'@param urls a vector of URLs
+#'
+#'@param strict whether or not to extract org handles; set to TRUE by default. This is mostly useful for referer
+#'parsing.
+#'
+#'@return a vector of hostnames or organisational handles.
+#'
+#'@importFrom httr parse_url
+#'
+#'@export
+host_handler <- function(urls, strict = TRUE){
+  
+  #Iconv
+  urls <- iconv(urls, to = "UTF-8")
+  
+  #Extract domain
+  urls <- unlist(lapply(urls,function(x){
+    
+    #Grab hostname
+    try_results <- try({expr = 
+                          hostname <- parse_url(x)$hostname
+    }, silent = TRUE)
+    
+    #Check - did it work?
+    if("try-error" %in% class(try_results)){
+      return("None")
+    }
+    
+    #If so, did it find anything?
+    if(is.null(hostname)){
+      return("None")
+    }
+    
+    #If not, return
+    return(hostname)
+  }))
+  
+  #Categorise
+  if(strict){
+    urls[grepl(x = urls, pattern = "((commons|meta|species)\\.((m|zero)\\.)?wikimedia\\.)|(wik(tionary|isource|ibooks|ivoyage|iversity|iquote|inews|ipedia|idata)\\.)",
+                   useBytes = TRUE, perl = TRUE)] <- "Internal"
+    urls[grepl(x = urls, pattern = "google(usercontent)?.", perl = TRUE, useBytes = TRUE)] <- "Google"
+    urls[grepl(x = urls, pattern = "yahoo.", fixed = TRUE)] <- "Yahoo"
+    urls[grepl(x = urls, pattern = "yandex.", fixed = TRUE)] <- "Yandex"
+    urls[grepl(x = urls, pattern = "baidu.", fixed = TRUE)] <- "Baidu"
+    urls[grepl(x = urls, pattern = "reddit.com", fixed = TRUE)] <- "Reddit"
+    urls[grepl(x = urls, pattern = "ask.com", fixed = TRUE)] <- "Ask"
+    urls[grepl(x = urls, pattern = "facebook.", fixed = TRUE)] <- "Facebook"
+    urls[grepl(x = urls, pattern = "aol.", fixed = TRUE)] <- "AOL"
+    urls[grepl(x = urls, pattern = "naver.", fixed = TRUE)] <- "Naver"
+    urls[grepl(x = urls, pattern = "duckduckgo.", fixed = TRUE)] <- "DuckDuckGo"
+    urls[grepl(x = urls, pattern = "sogou.", fixed = TRUE)] <- "Sogou"
+    urls[grepl(x = urls, pattern = "bing.", fixed = TRUE)] <- "Bing"
+    urls[grepl(x = urls, pattern = "daum.", fixed = TRUE)] <- "Daum"
+    urls[grepl(x = urls, pattern = "t.co", fixed = TRUE)] <- "Twitter"
+    urls[grepl(x = urls, pattern = "seznam.cz", fixed = TRUE)] <- "Seznam"
+    urls[grepl(x = urls, pattern = "startpage.", fixed = TRUE)] <- "Startpage"
+    urls[!urls %in% c("Internal","Google","Yahoo","Yandex","Baidu","Reddit","None",
+                              "Ask", "Facebook", "AOL", "Naver","DuckDuckGo","Sogou",
+                              "Bing","Daum","Twitter","Seznam","Startpage")] <- "Other"
+  }
+  
+  #Return
+  return(urls)
+}
