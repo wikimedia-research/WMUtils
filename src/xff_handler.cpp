04990d30358ab0841db3d35e72852210d7d10144
ironholds <ironholds@gmail.com>
2014-11-15 10:34:15 -0500

    make expandable

--- src/xff_handler.cpp
+++ src/xff_handler.cpp
@@ -4,9 +4,17 @@ using namespace Rcpp;
 //'@title xff_handler
 //'@description extracts the last IP address from a chain of IPs reported through the x_forwarded_for field
 //'
+//'@details our x_forwarded_for fields contain not just single IP addresses, but sometimes /chains/ of IP addresses,
+//'where users have gone through multiple levels of proxying. \code{xff_handler} extracts the earliest IP address in
+//'such chains or, when a chain is not present, simply returns the (only) IP found. It is called in the various
+//'geolocation functions in WMUtils, but may also be useful for extracting IPs in the case of (for example)
+//'generating pseudo-unique fingerprints.
+//'
 //'@param ips a vector of IP chains
 //'
-//'@return a vector of either the last IP in a chain, or the IP if no chain was present
+//'@return a vector of either the last IP in a chain, or the IP if no chain was present.
+//'
+//'@export
 // [[Rcpp::export]]
 std::vector< std::string > xff_handler(std::vector< std::string > ips) {
    
